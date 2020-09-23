2c3cc093f4c2a82269147d8fc14365f05cd540c2
shinyoshiaki <shinyoshiaki2012@gmail.com>
2020-09-23 18:24:37 +0900

    deploy client demo

--- demo/client/src/App.tsx
+++ demo/client/src/App.tsx
@@ -40,7 +40,7 @@ const App: FC = () => {
     const rtcManager = rtcManagerRef.current;
 
     if (!rtcManager) {
-      rtcManagerRef.current = new RTCManager("http://localhost:12222");
+      rtcManagerRef.current = new RTCManager("http://35.200.71.35:12222");
       init(rtcManagerRef.current);
       return;
     }
